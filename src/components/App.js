6f67b0557ada7ca2c208fa0bf9025547a44138fb
LioPeng <lionel.pengilley@hotmail.com>
2022-10-28 16:01:16 +1100

    second commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Contacts from './Contacts';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App animate__animated animate__fadeIn">
+        <Contacts />
+      </div>
+    );
+  }
+}
+
+export default App;
